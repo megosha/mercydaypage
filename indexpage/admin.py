f7b9edcfb22965405d58a32b7facc67bd57686ca
mvgosha <mvgosha@gmail.com>
2019-08-15 20:05:32 +0700

    base static, blick static, admin.py set up

--- indexpage/admin.py
+++ indexpage/admin.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
 from indexpage import models
 
+
 # Register your models here.
 class BlockAdmin(admin.ModelAdmin):
-    list_display = ['pk', ]
+    list_display = ['order', 'title', 'subtitle']
+    search_fields = ['order', 'title', 'subtitle', 'content']
 
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['pk']
+    list_display = ['order', 'title']
+    search_fields = ['order', 'title', 'content']
+
 
 class SettingsAdmin(admin.ModelAdmin):
-    list_display = ['pk']
+    list_display = ['title', 'metadescr', 'metakeywords']
+    search_fields = ['title', 'metadescr', 'metakeywords']
+
 
 admin.site.register(models.Block, BlockAdmin)
 admin.site.register(models.Item, ItemAdmin)
--- indexpage/models.py
+++ indexpage/models.py
@@ -4,12 +4,12 @@ from django.db import models
 # Create your models here.
 
 class Block(models.Model):
+    order = models.SmallIntegerField(verbose_name="Порядок отображения", default=0, blank=True, null=True)
     title = models.TextField(verbose_name="Заголовок (блока)", default='', blank=True, null=True)
     subtitle = models.TextField(verbose_name="Подзаголовок", default='', blank=True, null=True)
     content = models.TextField(verbose_name="Основной текст", default='', blank=True, null=True)
     picture = models.TextField(verbose_name="Фоновое изображение", default='', blank=True, null=True)
     item = models.ManyToManyField('Item', verbose_name="Элемент (списка, картинка)", blank=True)
-    #title = models.TextField(verbose_name="Заголовок (h1)", default='', blank=True, null=True)
 
     class Meta:
         verbose_name = "Блок"
