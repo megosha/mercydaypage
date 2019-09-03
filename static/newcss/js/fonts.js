4e25936d23ab8624fa0165a1fadce786236fd140
mvgosha <mvgosha@gmail.com>
2019-09-03 20:25:38 +0700

    major changes - new style, colors, bg images

---
+++ static/newcss/js/fonts.js
@@ -0,0 +1,13 @@
+"use strict";
+
+WebFont.load({
+    google: {
+        families: [
+            'Lora:400,700,latin,cyrillic,cyrillic-ext',
+            'Roboto:100,100italic,700,700italic,latin,cyrillic,cyrillic-ext',
+
+
+            'Open+Sans:400,700,700italic,400italic,latin,cyrillic,cyrillic-ext',
+        ]
+    }
+});
